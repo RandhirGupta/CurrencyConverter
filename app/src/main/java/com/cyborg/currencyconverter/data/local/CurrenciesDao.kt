84a1c9ce12fd4d8a5c2e7f106c75cd90cdf9f1ca
cyborg <randhirgupta92@gmail.com>
2019-10-13 16:58:18 +0530

    Fix : Conflict fix for insertion with primary key.

--- app/src/main/java/com/cyborg/currencyconverter/data/local/CurrenciesDao.kt
+++ app/src/main/java/com/cyborg/currencyconverter/data/local/CurrenciesDao.kt
@@ -1,15 +1,14 @@
 package com.cyborg.currencyconverter.data.local
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import android.database.sqlite.SQLiteConstraintException
+import androidx.room.*
 import com.cyborg.currencyconverter.data.entity.CurrenciesEntity
 import io.reactivex.Flowable
 
 @Dao
 interface CurrenciesDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertCurrencies(currenciesEntity: CurrenciesEntity)
 
     @Query("SELECT * FROM " + CurrenciesDatabase.TABLE_CURRENCY + " WHERE base = :baseCurrency")
